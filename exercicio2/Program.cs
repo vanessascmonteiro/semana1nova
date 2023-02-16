ca6e740f2bd35c88bc6f9fa30c9a97aa540056cc
Vanessa Monteiro <vanessascmonteiro@gmail.com>
2023-02-15 22:27:03 -0300

    exercico2

---
+++ exercicio2/Program.cs
@@ -0,0 +1,9 @@
+﻿float areabase, areaaltura, areatotal;
+
+Console.WriteLine("Qual o valor da área base?");
+areabase = float.Parse(Console.ReadLine());
+Console.WriteLine("Qual o valor da área altura?");
+areaaltura = float.Parse(Console.ReadLine());
+areatotal = ((areabase * areaaltura) /2);
+Console.WriteLine("O valor da área do triângulo é " + areatotal);
+
