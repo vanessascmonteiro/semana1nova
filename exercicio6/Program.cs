328f59ac704b00211d14816492f4ca0685b20c61
Vanessa Monteiro <vanessascmonteiro@gmail.com>
2023-02-15 23:09:26 -0300

    exercicio6

---
+++ exercicio6/Program.cs
@@ -0,0 +1,12 @@
+﻿int tabuada; 
+int num = 0;
+int resultado;
+
+Console.WriteLine("Informe o número da tabuada: "); 
+tabuada = int.Parse(Console.ReadLine());
+
+for(num = 0; num <= 10; num++){
+    resultado = (tabuada * num);
+    Console.WriteLine($"O resultado da tabuada é  " + resultado);
+
+}
