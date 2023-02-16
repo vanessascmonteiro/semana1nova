428ff72e9f319629adbaa088a5702f0c030aab2d
Vanessa Monteiro <vanessascmonteiro@gmail.com>
2023-02-15 22:21:46 -0300

    exercicio1

---
+++ exercicio1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Olá, tudo bem? Qual o seu nome?");
+string nome = Console.ReadLine();
+Console.WriteLine ("Qual o seu sobromenome?");
+string sobrenome = Console.ReadLine();
+Console.WriteLine ("Qual a sua idade?");
+int idade = int.Parse (Console.ReadLine ());
+Console.WriteLine ("Qual sua cor favorita?");
+string cor = Console.ReadLine();
+Console.WriteLine ( nome + " " + sobrenome + " " + "tem" + " " + idade + " anos e sua cor favorita é" + " " + cor);
+
