a0d29a2ec965223ac9b5efa2d976c9294df51c4f
Vanessa Monteiro <vanessascmonteiro@gmail.com>
2023-02-15 22:35:41 -0300

    exercicio3

---
+++ exercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿float nota1, nota2, nota3, media;
+
+Console.WriteLine("Digite a nota1");
+nota1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite a nota2");
+nota2 = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite a nota3");
+nota3 = float.Parse(Console.ReadLine());
+
+media = (nota1 + nota2 + nota3) /3;
+
+if(media>=6){
+Console.WriteLine("Você está aprovado");
+}
+else if(media>=5){
+    Console.WriteLine("Você está em recuperação");
+}
+else {
+Console.WriteLine("Você está reprovado");
+}
