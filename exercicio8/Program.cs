60bb34b6338a9b504a99cb080c471bac58027db0
Vanessa Monteiro <vanessascmonteiro@gmail.com>
2023-02-15 23:22:45 -0300

    exercicio8

---
+++ exercicio8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Sequência de Fibonacci, pressione enter para continuar");
+Console.ReadLine(); 
+
+int aux1 = 0;
+int aux2 =1; 
+int fibonaci;
+
+for(int i=0;i<20;i++){
+    fibonaci = aux1 + aux2;
+    Console.WriteLine(fibonaci);
+    aux1 = aux2;
+    aux2 = fibonaci;
+}
